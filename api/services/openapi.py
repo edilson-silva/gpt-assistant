5d979dcff1a9e31d423d8d7728e6c343e89a2afd
Edilson Silva <eddysilva1994@gmail.com>
2023-11-13 19:59:45 -0300

    fix: select and parse open ai response

--- api/services/openapi.py
+++ api/services/openapi.py
@@ -33,7 +33,7 @@ def get_summary(content: str) -> str:
         model="gpt-3.5-turbo",
     )
 
-    return chat_completion["choices"][0]["message"]["content"]
+    return str(chat_completion.choices[0].message.content)
 
 
 def get_answer(content: str, question: str) -> str:
@@ -61,4 +61,4 @@ def get_answer(content: str, question: str) -> str:
         model="gpt-3.5-turbo",
     )
 
-    return chat_completion["choices"][0]["message"]["content"]
+    return str(chat_completion.choices[0].message.content)
