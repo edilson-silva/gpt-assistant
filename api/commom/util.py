b500c4aa9c8b23d3300e5d47c071d8fc29103135
Edilson Silva <eddysilva1994@gmail.com>
2023-11-13 19:57:11 -0300

    fix: use correct html parser

--- api/commom/util.py
+++ api/commom/util.py
@@ -7,7 +7,7 @@ async def get_page_content(url: str) -> str:
     async with httpx.AsyncClient() as client:
         res = await client.get(url)
 
-    soup = BeautifulSoup(res.content, "html_parser")
+    soup = BeautifulSoup(res.content, "html.parser")
     page_content = soup.get_text()
 
     return page_content
